a82bbaae1c1bb314f76d7f222ff06ea04b21e252
Monke72 <wear16307@gmail.com>
2024-05-21 18:58:19 +0400

    added all sections for main page

--- script/script.js
+++ script/script.js
@@ -6,3 +6,24 @@ buttonLike.forEach((el) => {
     event.target.closest(".any").classList.toggle("any--active");
   }); //Поднимаемся по элемнтам выше и ищем елемент к лассом any когда находим добаувлем класс any--active
 });
+
+const about = document.querySelector(".about");
+
+function smoothScrollTo(element) {
+  const target = document.querySelector(element);
+  const positionY = target.offsetTop;
+  window.scrollTo({ top: positionY, behavior: "smooth" });
+}
+
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("btn__top")) {
+    smoothScrollTo(".header"),
+      setTimeout(() => {
+        about.classList.add("red");
+      }, 1500);
+
+    setTimeout(() => {
+      about.classList.remove("red");
+    }, 6000);
+  }
+});
