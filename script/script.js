c39681352d8b58465cc4c74255d7c2ebae8f7985
Monke72 <wear16307@gmail.com>
2024-06-09 18:29:05 +0400

    add srcipts

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+import { swiper } from "./swiper.js";
+
 const buttonLike = document.querySelectorAll(".any");
 console.log(buttonLike);
 
@@ -27,3 +29,86 @@ document.addEventListener("click", function (event) {
     }, 6000);
   }
 });
+
+export const todayBtn = document.querySelector(".today__view");
+
+function vievAll() {
+  todayBtn.classList.add("none");
+  document.querySelector(".today__swiper-buttons").classList.add("none");
+  document.querySelector(".today__wrapper").classList.remove("swiper-wrapper");
+  document.querySelector(".today__wrapper").classList.add("today__wrapper-st");
+  document.querySelector(".swiper").classList.remove("mySwiper2");
+}
+
+export { vievAll };
+
+todayBtn.addEventListener("click", vievAll);
+
+async function getProducts() {
+  const response = await fetch("products.json");
+  const data = await response.json();
+  return data;
+}
+
+const promiseToday = new Promise((resolve, reject) => {
+  resolve(getProducts());
+});
+
+promiseToday.then((data) => {
+  data.forEach((el) => {
+    const { img, discount, title, price } = el;
+    console.log(discount);
+    const adjastHtml = `
+    <div class="card swiper-slide ">
+              <div class="card__top">
+                <img 
+                  class="card__image anyToday" 
+                  src="./image/today/images/${img}"
+                  alt=""
+                  width="172px"
+                  height="160px"
+                />
+                <div class="card__sale">-${discount}%</div>
+                <div class="card-buttons">
+                  <button class="buttons__like card__btn">
+                    <svg
+                      class="any"
+                      width="17.000000"
+                      height="15.000000"
+                      viewBox="0 0 17 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                    >
+                      <path
+                        id="Vector"
+                        d="M0 3.88C0 5.61 0.7 9.73 7.59 13.88C7.71 13.96 7.85 14 8 14C8.14 14 8.28 13.96 8.4 13.88C15.3 9.73 16 5.61 16 3.88C16 1.73 14.2 0 12 0C9.79 0 8 2.35 8 2.35C8 2.35 6.2 0 4 0C1.79 0 0 1.73 0 3.88Z"
+                        stroke="#000000"
+                        stroke-opacity="1.000000"
+                        stroke-width="1.500000"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </button>
+                  <button class="buttons__show card__btn">
+                    <img src="./icon/today/show.svg" alt="" />
+                  </button>
+                </div>
+              </div>
+              <div class="card-btm">
+                <div class="card-btm__name">${title}</div>
+                <div class="card-btm__price">
+                  $ ${
+                    price - (price / 100) * discount
+                  } <span class="old--price"> $ ${price}</span>
+                </div>
+              </div>
+            </div>
+    `;
+    document
+      .querySelector(".today__wrapper")
+      .insertAdjacentHTML("beforeend", adjastHtml);
+  });
+});
+
+// elInHtml();
--- script/swiper.js
+++ script/swiper.js
@@ -1,23 +1,29 @@
-var swiper = new Swiper(".mySwiper", {
+import { todayBtn } from "./script.js";
+
+export let swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
   },
 });
 
-var swiper = new Swiper(".mySwiper2", {
+var swiper2 = new Swiper(".mySwiper2", {
   slidesPerView: 4,
   spaceBetween: 30,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  simulateTouch: null,
 });
 
-var swiper = new Swiper(".mySwiper3", {
+var swiper3 = new Swiper(".mySwiper3", {
   slidesPerView: 6,
   spaceBetween: 30,
   navigation: {
     nextEl: ".swiper--cat-next",
     prevEl: ".swiper--cat-prev",
   },
+});
+todayBtn.addEventListener("click", () => {
+  swiper2.simulateTouch = false;
 });
